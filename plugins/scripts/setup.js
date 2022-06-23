b608833f033f3bd2896ed7fd607fd796fa986eb5
Antoine-LE-BORGNE <le-borgne.e1903763@etud.univ-ubs.fr>
2022-06-23 11:13:24 +0200

    final product

--- plugins/scripts/setup.js
+++ plugins/scripts/setup.js
@@ -1,10 +1,12 @@
 
 const _addTheme = (typeof module !== 'undefined')? require("./menuAddon").addTheme : addTheme;
 
+let _setup;
+
 /**
- *
+ * this contains the default setup of the editor
  */
-function setup(){
+function defaultSetup(){
     //Theming
     _addTheme("dark", "dark-theme");
     _addTheme("light", "light-theme");
@@ -12,12 +14,25 @@ function setup(){
 
 }
 
+/**
+ * create a custom setup that can be added after the default one
+ *
+ * @param {Function} setup callback for the custom setup
+ */
+function createSetup(setup){
+    _setup = setup;
+}
 
+/**
+ * Execute the setups, the default one and then the custom one (if it exists)
+ */
 function executeSetup(){
-    setup();
+    defaultSetup();
+    if(_setup)_setup();
 }
 
 
 if(typeof module !== 'undefined'){//for test purposes
     module.exports.executeSetup = executeSetup;
+    module.exports.createSetup = createSetup;
 }
--- plugins/test/test.js
+++ plugins/test/test.js
@@ -1,14 +1,15 @@
 const {buildSchema} =  require("../scripts/schema");
 const {ProseMirror} = require("../scripts/proseMirror")
-const {buildMenu} = require("../scripts/menuAddon");
+const {buildMenu, addTheme, applyTheme} = require("../scripts/menuAddon");
 const {buildEditor} = require("../scripts/build");
 const {translateToBBCode} = require("../scripts/functionAddon");
+const {executeSetup} = require("../scripts/setup");
 
 describe("TEST - ProseMirror custom adaptation", () => {
 
     describe('Schema builder', () => {
 
-        it('Should have a schema usable', () => {
+        it('Should have a usable schema', () => {
 
             let editor = {}
 
@@ -17,6 +18,7 @@ describe("TEST - ProseMirror custom adaptation", () => {
             expect(editor.schema).not.toBeNull()
 
         })
+
         it('Should have bold, italic and image', () => {
 
             let editor = {}
@@ -29,6 +31,17 @@ describe("TEST - ProseMirror custom adaptation", () => {
 
         })
 
+        it('Should have italic, underline and blockquote', () => {
+
+            let editor = {}
+
+            buildSchema(ProseMirror, editor, ['italic', 'underline', 'blockquote']);
+
+            expect(editor.schema.marks.italic).not.toBeNull()
+            expect(editor.schema.marks.underline).not.toBeNull()
+            expect(editor.schema.nodes.blockquote).not.toBeNull()
+
+        })
     })
 
 
@@ -45,11 +58,35 @@ describe("TEST - ProseMirror custom adaptation", () => {
 
         })
 
+        it('Should have four sections', () => {
+
+            let editor = {}
+
+            buildSchema(ProseMirror, editor, ['bold', 'italic', 'image']);
+            buildMenu(ProseMirror, editor, ['bold', 'italic', 'image']);
+
+            expect(editor.menu.fullMenu.length).toBe(4)
+
+        })
+
+        it('Should have 3 marks', () => {
+
+            let editor = {}
+
+            buildSchema(ProseMirror, editor, ['bold', 'italic', 'image']);
+            buildMenu(ProseMirror, editor, ['bold', 'italic', 'image']);
+
+            expect(editor.menu.fullMenu[0].length).toBe(2)
+
+        })
+
     })
+
+
 
     describe('BBCode parser', () => {
 
-        it('Should parse simples marks ("<x>test</x>" into "[x]test[/x]")', () => {
+        it('Should parse simples marks (ex: "<x>test</x>" into "[x]test[/x]")', () => {
 
             expect(translateToBBCode('<p>test</p>')).toBe("test")
             expect(translateToBBCode('<i>test</i>')).toBe("[i]test[/i]")
@@ -66,7 +103,6 @@ describe("TEST - ProseMirror custom adaptation", () => {
             expect(translateToBBCode('<code>test</code>')).toBe("[code]test[/code]")
             expect(translateToBBCode('<blockquote>test</blockquote>')).toBe("[quote]test[/quote]")
 
-
         })
 
         it('Should parse more complex marks ("ex: <img src="test">" into "[img]test[/img]")', () => {
@@ -78,8 +114,11 @@ describe("TEST - ProseMirror custom adaptation", () => {
 
         it('Should parse in complex situation ("ex: <img src="test"><li><b><ul>test</ul></b></li>" into "[img]test[/img][*][b][list]test[/list][/b]")', () => {
 
-            expect(translateToBBCode('<img src="test"><li><b><ul>test</ul></b></li>')).toBe("[img]test[/img][*][b][list]test[/list][/b]")
-            expect(translateToBBCode('<a href="h1">test</a>')).toBe("[url=h1]test[/url]")
+            expect(translateToBBCode('<img src="test"><li><b><ul>test</ul></b></li>'))
+                .toBe("[img]test[/img][*][b][list]test[/list][/b]")
+
+            expect(translateToBBCode('<a href="class"><p><b><i><a href="test"></a></i><i><b>test</b></i></b></p></a>'))
+                .toBe("[url=class][b][i][url=test][/url][/i][i][b]test[/b][/i][/b][/url]")
 
         })
 
@@ -88,14 +127,63 @@ describe("TEST - ProseMirror custom adaptation", () => {
 
     describe('Theme', () => {
 
+        it('Should be able to add a usable theme', () => {
+
+            addTheme("customTheme", "custom-theme")
+
+            let fakeEditor = document.createElement('div');
+
+            applyTheme("customTheme", fakeEditor)
+
+            expect(fakeEditor.classList.contains("custom-theme")).toBe(true);
+
+        })
+
         it('Should have the dark theme', () => {
 
-            let editor = {}
+            document.body.innerHTML =
+                '<div prosemirror-editor="editor_1" prosemirror-theme="dark" prosemirror-available-style="bold;underline;italic;list;emoji;video-link"></div>\n' +
+                '<div prosemirror-content-of="editor_1"></div>'
 
-            buildSchema(ProseMirror, editor, ['bold', 'italic', 'image']);
-            buildMenu(ProseMirror, editor, ['bold', 'italic', 'image']);
+            let editor = document.querySelector('[prosemirror-editor]')
 
-            expect(editor.menu).not.toBeNull()
+            executeSetup();
+
+            buildEditor(ProseMirror, editor)
+
+            expect(document.querySelector('.dark-theme')).not.toBeNull();
+
+
+        })
+
+        it('Should have the light theme', () => {
+
+            document.body.innerHTML =
+                '<div prosemirror-editor="editor_1" prosemirror-theme="light" prosemirror-available-style="bold;underline;italic;list;emoji;video-link"></div>\n' +
+                '<div prosemirror-content-of="editor_1"></div>'
+
+            let editor = document.querySelector('[prosemirror-editor]')
+
+            buildEditor(ProseMirror, editor)
+
+            expect(document.querySelector('.light-theme')).not.toBeNull();
+
+
+        })
+
+
+        it('Should have the auto theme', () => {
+
+            document.body.innerHTML =
+                '<div prosemirror-editor="editor_1" prosemirror-theme="auto" prosemirror-available-style="bold;underline;italic;list;emoji;video-link"></div>\n' +
+                '<div prosemirror-content-of="editor_1"></div>'
+
+            let editor = document.querySelector('[prosemirror-editor]')
+
+            buildEditor(ProseMirror, editor)
+
+            expect(document.querySelector('.auto-theme')).not.toBeNull();
+
 
         })
 
@@ -115,6 +203,21 @@ describe("TEST - ProseMirror custom adaptation", () => {
             buildEditor(ProseMirror, editor)
 
             expect(document.querySelector('[prosemirror-editor]').children.length).toBeGreaterThan(0)
+
+        })
+
+        it('Should have a complete menu set', () => {
+
+            document.body.innerHTML =
+                '<div prosemirror-editor="editor_1" prosemirror-theme="dark" prosemirror-available-style="bold;underline"></div>\n' +
+                '<div prosemirror-content-of="editor_1"></div>'
+
+            let editor = document.querySelector('[prosemirror-editor]')
+
+            buildEditor(ProseMirror, editor)
+
+
+            expect(document.querySelectorAll('.ProseMirror-menuitem').length).toEqual(9)
 
         })
 
